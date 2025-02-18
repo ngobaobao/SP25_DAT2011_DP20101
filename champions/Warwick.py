46b7f93db2aea1cdf42409bf35ff1e61474affe1
ngobaobao <huyoicolen00@gmail.com>
2025-02-18 16:04:33 +0700

    Update Warwick.py

--- champions/Warwick.py
+++ champions/Warwick.py
@@ -5,12 +5,9 @@ class Warwick(Champion):
         
     def cast_skill_q(self):
         return 'Cắn xé'
-    
     def cast_skill_w(self):
         return 'Mùi máu'
-    
     def cast_skill_e(self):
         return 'Gầm thét'
-    
     def cast_skill_r(self):
         return 'Khóa chết'
