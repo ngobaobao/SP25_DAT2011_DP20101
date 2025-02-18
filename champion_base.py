70cdef8bfca5d1b8b731afec4eca9272bc133351
phongtt35 <phongtt35@fe.edu.vn>
2025-02-18 15:03:04 +0700

    add base cast skill

--- champion_base.py
+++ champion_base.py
@@ -14,6 +14,15 @@ class Champion(ABC):
     def attack(self):
         print(self.__name, 'thi triển' ,self.__skill)
 
+    def cast_skill_q(self):
+        pass
+    def cast_skill_w(self):
+        pass
+    def cast_skill_r(self):
+        pass
+    def cast_skill_e(self):
+        pass
+
     # Getter
     def get_name(self): # Cho phép bên ngoài xem được giá trị thuộc tính private
         return self.__name
